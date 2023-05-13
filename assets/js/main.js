aff5dcf852fcf072b3dead1d254b5d2b9d6199ca
Marlon Pilco (Bedimcode) <bedimcode@gmail.com>
2023-05-12 19:09:56 -0500

    Added source code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
 /*=============== ACTIVE LINK ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkColor(){
+    navLink.forEach(link => link.classList.remove('active-link'))
+    this.classList.add('active-link')
+}
+navLink.forEach(link => link.addEventListener('click', linkColor))
